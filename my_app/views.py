c560c931e37ee4d718e3060b94dce27dc2a042fd
medha97 <medha.medhagupta@gmial.com>
2019-11-16 00:45:50 +0530

    importing urls, title, price

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,42 @@
 import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
 
 # Create your views here.
+BASE_CRAIGSLIST_URL = "https://delhi.craigslist.org/search/ggg?query={}"
+
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    # print(quote_plus(search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result_price'):
+            post_price = post.find(class_='result_price').text
+        else:
+            post_price = 'N/A'
+        final_postings.append((post_title, post_url, post_price))
+
+
+
+    # print(data)
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
